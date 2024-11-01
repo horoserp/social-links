4a4a2c841738c280eb7f5e205d2dd9f6a86d9c19
Robert Horosewski <horoserp@yahoo.com>
2024-11-01 03:43:31 -0400

    Update: Add Link Titles for accessibility.

--- src/App.js
+++ src/App.js
@@ -8,17 +8,31 @@ function App() {
       <h1>Robert Horosewski</h1>
       <h2>Nashua, New Hampshire, USA</h2>
       <h3>"Frontend Developer who loves to code!"</h3>
-      <Links path="https://www.linkedin.com/in/robert-horosewski-8a0608196/">
+      <Links
+        path="https://www.linkedin.com/in/robert-horosewski-8a0608196/"
+        title="Visit my LinkedIn Page"
+      >
         LinkedIn
       </Links>
-      <Links path="https://horoserp.github.io/React-Portfolio/">
+      <Links
+        path="https://horoserp.github.io/React-Portfolio/"
+        title="Visit my Portfolio Page"
+      >
         Portfolio
       </Links>
-      <Links path="https://github.com/horoserp/">GitHub</Links>
-      <Links path="https://www.frontendmentor.io/profile/horoserp">
+      <Links path="https://github.com/horoserp/" title="Visit my GitHub Page">
+        GitHub
+      </Links>
+      <Links
+        path="https://www.frontendmentor.io/profile/horoserp"
+        title="Visit my Frontend Mentor Page"
+      >
         Frontend Mentor
       </Links>
-      <Links path="https://stackoverflow.com/users/12617284/horoserp">
+      <Links
+        path="https://stackoverflow.com/users/12617284/horoserp"
+        title="Visit my Stack Overflow Page"
+      >
         Stack Overflow
       </Links>
     </main>
--- src/Components/Links.js
+++ src/Components/Links.js
@@ -5,7 +5,12 @@ import { BrowserRouter as Router } from "react-router-dom";
 function Links(props) {
   return (
     <Router>
-      <Link to={props.path} target="_blank" rel="noopener noreferrer">
+      <Link
+        to={props.path}
+        target="_blank"
+        rel="noopener noreferrer"
+        title={props.title}
+      >
         <div className="links">{props.children}</div>
       </Link>
     </Router>
